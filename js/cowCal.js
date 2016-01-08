ff06862ba0ffb55032ec38f9fd4a1507419faee3
Dave Russell Jr <dave@createazure.com>
2016-01-08 10:24:17 +0000

    Add cowCal.prototype.month() to generate months individually
    This commit will change the original generateCalendar() function
    to cowCal() and then add a prototype to generate months. However,
    the prototype doesn't exactly work right now.. so the next commit
    will fix that.
    
    Signed-off-by: Dave Russell Jr <dave@createazure.com>

--- js/cowCal.js
+++ js/cowCal.js
@@ -1,8 +1,12 @@
 /*
  * cowCal Javascript calendar plugin
- * Author: Dave Russell (drussell393)
+ * Author: Dave Russell Jr (drussell393)
  * License: MIT
  */
+
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
 
 function convertTimezone(remoteUTCOffset) {
     // Handle our local time
@@ -13,37 +17,81 @@ function convertTimezone(remoteUTCOffset) {
     var remoteTime = new Date(localUTC + (3600000 * remoteUTCOffset));
     
     // Return our converted time
-    return remoteTime.toLocaleString();
+    return remoteTime;
 }
 
-function generateCalendar(remoteUTCOffset, month, year) {
-    // Set our available sizes
-    var sizeArray = ['day', '4days', 'week', 'month', 'year'];
-
+function cowCal(remoteUTCOffset, month, year, firstDayofWeek) {
     // Get our current date/time
     var currentTime = convertTimezone(remoteUTCOffset);
-    
+
     // Defining calendar presets
-    this.cowNamesOfDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    this.cowNamesOfMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.namesOfDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var cowNamesOfMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.month = ((isNaN(month) || month == null) ? currentTime.getMonth() : month);
     this.year = ((isNaN(year) || year == null) ? currentTime.getFullYear() : year);
-    var size = ((isInArray(size, sizesArray)) ? size : 'month');
+    this.firstDayofWeek = ((isNaN(firstDayofWeek) || firstDayofWeek == null) ? 0 : firstDayofWeek)
     // Check for leap years
     if (((this.year % 4) == 0) || ((this.year % 100) == 0) && ((this.year % 400) == 0)) {
         var cowDaysInMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     } else {
         var cowDaysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     }
-
     this.firstDayofMonth = new Date(this.year, this.month, 1).getDay();
     this.daysInMonth = cowDaysInMonths[this.month];
-    this.nameOfMonth = cowNamesOfMonth[this.month];
-    // Month Calendar
-    if (this.size == 'month') {
-
-        // Find general information about this month (position of first day, days in it, and name of the month)
-    }
+    this.nameOfMonth = cowNamesOfMonths[this.month];
 }
 
+cowCal.prototype.month = function() {
+    var htmlOutput;
+    htmlOutput += '<table class="cowCal-month">';
+    htmlOutput += '<tr>';
+    var i = this.firstDayofWeek;
 
+    // Generate our labels for the days of the week
+    if (i > 0) {
+        for (i < 7; i++;) {
+            htmlOutput += '<th>' + this.namesOfDays[i] + '</th>';
+        }
+        var i = 0;
+        for (i < this.firstDayofWeek; i++;) {
+            htmlOutput += '<th>' + this.namesOfDays[i] + '</th>';
+        }
+    }
+    else
+    {
+        console.log(i);
+        htmlOutput += '<th>' + this.namesOfDays[i] + '</th>';
+    }
+    htmlOutput += '</tr>';
+    
+    // Generate our actual days in the month
+    var dayNumber = 1;
+    var week = 0;
+    var dayPosition = 0;
+
+    for (week < 9; week++;) {
+        htmlOutput += '<tr>';
+        for (dayPosition <= 6; dayPosition++;) {
+            htmlOutput += '<td>';
+            if ((dayNumber <= this.daysInMonth) && (week > 0 || dayPosition >= this.firstDayofWeek)) {
+                htmlOutput += dayNumber;
+                dayNumber++;
+            }
+            htmlOutput += '</td>';
+        }
+        if (dayNumber > this.daysInMonth) {
+            break;
+        }
+        else
+        {
+            htmlOutput += '</tr>';
+            htmlOutput += '<tr>';
+        }
+    }
+    htmlOutput += '</tr>';
+    htmlOutput += '</table';
+
+    // Make htmlOutput part of the prototype/object
+    this.htmlOutput = htmlOutput;
+}
+            
